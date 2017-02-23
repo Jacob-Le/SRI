ee445047dca0078172345f21c515d56fcc27e2b2
Jacob <jacob.phong.le@gmail.com>
2017-02-22 22:07:48 -0800

    changes

--- src/Rule.cpp
+++ src/Rule.cpp
@@ -4,11 +4,31 @@
 #include <cstdarg.h>
 #include "Fact.h"
 
-template <typename T>
-Rule::Rule(std::string n, int count, ...){
+template <typename T, typename O>
+Rule::Rule(std::string n, const &std::vector<O*> Ops, int count, ...){
   std::string name = n;
   va_list cmp;
   std::vector<T> components;
   for(int i = 0; i < count, i++) components.pushback(va_arg(cmp, T));
+  std::vector<O*> ops = Ops;
   va_end(cmp);
+}
+
+boolean Rule::evaluate(){
+  std::vector<bool> truths;
+	for(const auto & r : components) truths.push_back(r.evaluate());
+  int count = 0
+  for(const auto& bool a: truths){
+  	if(a != boolList[count]) return false; //This would account for ANDs and ORs CHANGE THIS
+    count++;
+  }
+  return true;
+}
+
+bool Rule::enact(){
+  if(evaluate()){
+    //add to KB here
+    return true;
+  }
+  return false;
 }
--- src/Rule.h
+++ src/Rule.h
@@ -4,36 +4,24 @@
 #include <cstdarg.h>
 #include "Fact.h"
 
-template<typename T>
+template<typename T, typename O>
 typedef struct Rule{
   std::string name;
-  T * e1;
-  T * e2;
 
-//OR
+  //Vector of predicate components (Rules or facts) that make up this rule
   std::vector<T*> components;
 
   //Function pointer that points to a function that emulates boolean operators
-  Ops * op;
+  std::vector<O*> ops;
 
   //Variadic constructor that accepts any number of conditions
-  Rule(std::string name, int count, ...);
+  Rule(std::string name, const &std::vector<O*> ops, int count, ...);
   //copy and move constructors: later!
 
+  bool evaluate();
+  bool enact();
+
   //Operator overloads: shortcuts for calculating condition fufullment
-  boolean operator &&(const &Rule);
-  boolean operator ||(const &Rule);
+  bool operator &&(const &Rule);
+  bool operator ||(const &Rule);
 }Rule;
-
-
-x Grandparent z = (x father y && y father z) || y father z
-
-isRuleTrue = rule.evalute() // truthValues looks like [TRUE, TRUE, FALSE]
-
-ruleIsTrue truthValues[0] rule.operator[0] truthValues[1] rule.operator[1]
-
-
-
-----
-
-MYFIRSTRULE: PRED1 && PRED2 || PRED3 --> PRED4
