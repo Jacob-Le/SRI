192afd1d6a8ab0059cebc4f3cf78601525fa99d5
Alexander Mao <acmao@ucsc.edu>
2017-02-23 22:16:12 -0800

    Added implementation for RB. Still need file loading.

--- src/RB.h
+++ src/RB.h
@@ -7,10 +7,11 @@ private:
   std::vector<Rule> rules;
 public:
   RB();
+  RB(const RB &otherRB);
   ~RB();
 
-  void Add(Rule * r);
-  Rule Remove(std::string name);
+  void Add(Rule::predicate * r);
+  Rule* Remove(std::string name);
   std::string toString();
   void Load(std::string filepath);
   std::string Dump(std::string filepath);
