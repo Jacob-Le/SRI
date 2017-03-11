8a270e9c2f5308d46c1a4d2154b02d443be58ce9
Jacob <jacob.phong.le@gmail.com>
2017-03-10 23:41:27 -0800

    A new direction

--- src/KB.h
+++ src/KB.h
@@ -1,21 +1,37 @@
-#pragma once
-
-#include <stdlib.h>
-#include <vector>
+//Knowledge Base//
+#include<stdlib.h>
+#include<vector>
 #include<map>
 
 using namespace std;
 
-class KB{
-public:
-	//map, stored facts categorized by relationship
-	std::map <string, vector<Fact*> > FactMap;
-	KB();
-	~KB();
-
-	bool Add(Fact * fact);
-	Fact* Remove(Fact * fact);
-	void Remove(string f);
-	vector<Fact*>* Find(string findKey);
-	string toString();
+template<typename Key, typename Value, unsigned int N> //unsigned
+struct nestedMap{
+	typedef map<Key, typename nestedMap<Key, Value, N-1>::type> type;
 };
+
+template<typename Key, typename Value>
+struct nestedMap<Key, Value, 1>{
+	typedef map<Key, Value> type;
+};
+
+template<typename Key, typename Value, unsigned int N> //unsigned
+using FactMap = typename nestedMap<Key, Value, N>::type; 
+// USE LIKE THIS FactMap<std::string, Value, N> mapName;
+
+
+class KB{
+public:	
+	const static int index = 2;
+	FactMap<string, vector<string>, KB::index> facts;	
+	
+
+	KB();
+	
+	void Add(vector<string> fact);
+	//void Add(FactMap<string, vector<string>, int> facts, vector<string> actors, int depth);
+	void Remove(string factName);
+	string toString();
+	
+};
+
--- src/Query.h
+++ src/Query.h
@@ -1,6 +1,10 @@
-#include "Predicate.h"
-#include "KB.h"
-#include "RB.h"
+#pragma once
+#include <string>
+#include <vector>
+#include "RB.cpp"
+#include "KB.cpp"
+
+//#include "Predicate.cpp"
 
 using namespace std;
 
@@ -11,6 +15,8 @@ private:
 	RB * rb;
 public:
 	//something var currentQuery <--THE THING THAT QUERY PASSES AROUND IDK WHAT IT IS THO
+	vector<string> predNames; //Relevant relationship names
+	vector<string> ToBind; //Variables that need actors
 
 	Query(KB * kb, RB * rb);
 
@@ -18,12 +24,21 @@ public:
 	vector<Fact*>* listFact(KB* Knowledge, string factKey); //used to return vector<string>
 
 	//SIMPLE RULE-------------------------------------------------------------------
-	void listSimpleRule(/*something*/);
+	//void listSimpleRule(/*something*/);
 	void printResults();
 	vector<Fact*> concatenate(vector<Fact*>* resultA, vector<Fact*>* resultB);
-	vector<Fact*> preventDupes(vector<Fact*>* A, vector<Fact*> B);
+	//vector<Fact*> preventDupes(vector<Fact*>* A, vector<Fact*> B);
 
+	void inference(vector<string> newFact);
+	bool ruleEvaluate(Rules * r, vector<string> actors);
+	bool ruleEvalHelper(string name, vector<string> actors);
+
+	//void CreatePredNames(Rule* r);
+	//vector< vector<Fact*>*> PermutateAndBind(KB* kb);
+	//vector<string> Bind(KB* KnowledgeBase);
+	//vector<int> BuildID();
 
 	//Part 3: evaluate Rules -------------------------------------------------------
 	bool enact(Rule * r, vector<string> s, KB * kb);
+
 };
