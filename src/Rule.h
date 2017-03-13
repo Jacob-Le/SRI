bc645a886e60726f27c1b4b7f3bc79c363a7dc96
Jacob <jacob.phong.le@gmail.com>
2017-03-12 18:41:52 -0700

    rule changes

--- src/Rule.h
+++ src/Rule.h
@@ -6,9 +6,12 @@
 using namespace std;
 
 //Rules are made up of components (which can be either other rules or facts)
-typedef struct Rule : Predicate{
+typedef struct Rule{
 
-  string Relationship;
+  //String name of the rule
+  string name;
+  //Logical operator represented by an int (OR = 0, AND = anything else)
+  int ops;
   //Vector of strings representative of actors involved in the rule
   vector<string> actors;
   //Vector of predicate components (Rules or facts) that make up this rules
@@ -16,7 +19,7 @@ typedef struct Rule : Predicate{
 
   Rule();
   //Variadic constructor that accepts any number of conditions
-  Rule(string name, const vector<string> actors, vector<string> cmps);
+  Rule(string name, int ops, const vector<string> actors, vector<string> cmps);
   //copy and move constructors
   Rule(const Rule & r);
   Rule(Rule && r);
